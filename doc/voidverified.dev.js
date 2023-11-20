45c8a179da2c6616944b979ec89336e040bbd621
voidnyan <voidnyan@proton.me>
2023-11-20 19:46:54 +0200

    dev: add development userscript

---
+++ doc/voidverified.dev.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         VoidVerifiedDev
+// @namespace    http://tampermonkey.net/
+// @version      DEV
+// @description  try to take over the world!
+// @author       You
+// @match        https://anilist.co/*
+// @require      file://C:/dev/void-verified/dist/voidverified.user.js
+// @grant        none
+// ==/UserScript==
