1ef5af1784488af19f603531eabc8f057c27cb77
voidnyan <voidnyan@proton.me>
2024-02-16 21:40:32 +0200

    test(Toaster): add more testcases

--- tests/utils/toaster.test.js
+++ tests/utils/toaster.test.js
@@ -16,29 +16,31 @@ afterAll(() => {
 
 const renderSettingsUi = jest.fn();
 
+const toastCases = [
+	[toastLevels.info, toastTypes.info, true],
+	[toastLevels.info, toastTypes.success, true],
+	[toastLevels.info, toastTypes.warning, true],
+	[toastLevels.info, toastTypes.error, true],
+	[toastLevels.success, toastTypes.info, false],
+	[toastLevels.success, toastTypes.success, true],
+	[toastLevels.success, toastTypes.warning, true],
+	[toastLevels.success, toastTypes.error, true],
+	[toastLevels.warning, toastTypes.info, false],
+	[toastLevels.warning, toastTypes.success, false],
+	[toastLevels.warning, toastTypes.warning, true],
+	[toastLevels.warning, toastTypes.error, true],
+	[toastLevels.error, toastTypes.info, false],
+	[toastLevels.error, toastTypes.success, false],
+	[toastLevels.error, toastTypes.warning, false],
+	[toastLevels.error, toastTypes.error, true],
+];
+
 describe("Toaster", () => {
 	beforeEach(() => {
 		document.body.replaceChildren();
 	});
 
-	test.each([
-		[toastLevels.info, toastTypes.info, true],
-		[toastLevels.info, toastTypes.success, true],
-		[toastLevels.info, toastTypes.warning, true],
-		[toastLevels.info, toastTypes.error, true],
-		[toastLevels.success, toastTypes.info, false],
-		[toastLevels.success, toastTypes.success, true],
-		[toastLevels.success, toastTypes.warning, true],
-		[toastLevels.success, toastTypes.error, true],
-		[toastLevels.warning, toastTypes.info, false],
-		[toastLevels.warning, toastTypes.success, false],
-		[toastLevels.warning, toastTypes.warning, true],
-		[toastLevels.warning, toastTypes.error, true],
-		[toastLevels.error, toastTypes.info, false],
-		[toastLevels.error, toastTypes.success, false],
-		[toastLevels.error, toastTypes.warning, false],
-		[toastLevels.error, toastTypes.error, true],
-	])(
+	test.each(toastCases)(
 		"toast level %p should render %p message %p",
 		(toastLevel, toastType, shouldRender) => {
 			configureToaster(toastLevel);
@@ -53,17 +55,63 @@ describe("Toaster", () => {
 		}
 	);
 
+	test.each(toastCases)(
+		"toast level %p shouldn't render when toaster is disabled",
+		(toastLevel, toastType) => {
+			configureToaster(toastLevel, false);
+			raiseToast(toastType, toastLevel);
+
+			const toast = document.querySelector(".void-toast");
+			expect(toast).toBeNull();
+		}
+	);
+
+	it("renders critical error", () => {
+		configureToaster(3, false);
+		Toaster.critical("critical error");
+
+		const toast = document.querySelector(".void-toast");
+		expect(toast).not.toBeNull();
+	});
+
+	it("handles null config", () => {
+		const settings = new Settings();
+		settings.options.toasterEnabled.value = true;
+		const toasterConfig = new ToasterConfig();
+		localStorage.setItem(
+			"void-verified-toaster-config",
+			JSON.stringify(toasterConfig)
+		);
+		Toaster.initializeToaster(settings);
+
+		Toaster.error("test");
+		const toast = document.querySelector(".void-toast");
+		expect(toast).not.toBeNull();
+	});
+
+	it("gets removed after a set amount of time", () => {
+		jest.useFakeTimers();
+		jest.spyOn(global, "setTimeout");
+		configureToaster(2);
+		Toaster.error("test");
+		jest.runAllTimers();
+		const toast = document.querySelector(".void-toast");
+		expect(toast).toBeNull();
+		jest.useRealTimers();
+	});
+});
+
+describe("Toaster Settings", () => {
 	it("renders settings", () => {
 		configureToaster(0);
-
 		const container = Toaster.renderSettings(renderSettingsUi);
 		expect(container).toMatchSnapshot();
 	});
 });
 
-const configureToaster = (toastLevel) => {
+const configureToaster = (toastLevel, enabled = true) => {
 	const settings = new Settings();
-	settings.options.toasterEnabled.value = true;
+	settings.options.toasterEnabled.value = enabled;
 	const toasterConfig = new ToasterConfig({ toastLevel });
 	localStorage.setItem(
 		"void-verified-toaster-config",
