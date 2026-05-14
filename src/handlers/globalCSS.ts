72e2ef380613ce9db6e5f1803614282463e3874d
voidnyan <147516552+voidnyan@users.noreply.github.com>
2026-05-14 15:43:04 +0300

    fix: remove ace editor

--- src/handlers/globalCSS.ts
+++ src/handlers/globalCSS.ts
@@ -2,9 +2,6 @@ import { StyleHandler } from "./styleHandler";
 import {LocalStorageKeys} from "../assets/localStorageKeys";
 import {StaticSettings} from "../utils/staticSettings";
 import {DOM} from "../utils/DOM";
-import {AceEditorInitializer} from "../utils/aceEditorInitializer";
-import {Button} from "../components/components";
-
 export class GlobalCSS {
 	static styleHandler;
 	static styleId = "global-css";
@@ -38,12 +35,13 @@ export class GlobalCSS {
 		const label = DOM.create("h3", null, `Custom ${cssName} CSS`);
 		container.append(label);
 
-
-		const editor = AceEditorInitializer.createEditor(`custom-css-editor-${cssName}`, this.css);
-		container.append(editor);
-		AceEditorInitializer.addChangeHandler(`custom-css-editor-${cssName}`, (value) => {
-			this.updateCss(value);
+		const textarea = DOM.create<HTMLTextAreaElement>("textarea", "textarea");
+		textarea.value = this.css;
+		textarea.addEventListener("input", (event: any) => {
+			this.updateCss(event.target.value);
 		});
+
+		container.append(textarea);
 
 		if (cssName === "global") {
 			const notice = DOM.create("div");
@@ -53,21 +51,6 @@ export class GlobalCSS {
 			container.append(notice);
 		}
 
-		const prettifyButton = Button("Prettify", () => {
-			// @ts-ignore
-			const beautify = ace.require("ace/ext/beautify");
-			// @ts-ignore
-			const editor = ace.edit(`void-custom-css-editor-${cssName}`);
-			const value = editor.getValue()
-				.replace(/(\n\s*\n)+/g, '\n\n')
-				.replace(/\{[^\}]*\}/g, (block) => {
-					// Remove all empty lines within the block
-					return block.replace(/\n\s*\n/g, '\n');
-				});
-			editor.setValue(value);
-			beautify.beautify(editor.session);
-		});
-		container.append(prettifyButton);
 		return container;
 	}
 
