53d578ac6281d7846216dd347da43a46b6ea9b71
voidnyan <147516552+voidnyan@users.noreply.github.com>
2026-08-03 18:38:45 +0300

    fix(MediaOverview): fix rendering when season data is missing

--- src/assets/changeLog.js
+++ src/assets/changeLog.js
@@ -18,7 +18,8 @@ class Feature {
 
 export const changeLog = [
 	new Version("2.2", [
-		new Feature("Display media overview when hovering VoidVerified In Progress entries. Enable this in the Mini Media subcategory in VoidVerified settings.")
+		new Feature("Display media overview when hovering VoidVerified In Progress entries. Enable this in the Mini Media subcategory in VoidVerified settings."),
+		new Feature("Fixed some cases where Media Overview failed to render due to data field not existing.")
 	]),
 	new Version("2.1", [
 		new Feature("Change list activity settings from the in-progress section settings in home feed.")
